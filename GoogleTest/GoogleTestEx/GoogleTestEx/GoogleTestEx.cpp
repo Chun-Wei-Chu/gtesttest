2972831eddf98542517c9a68534e82f412c2cf4c
MaxChu <maxchu@moneydj.com>
2017-06-03 10:11:18 +0800

    *init: gtest project

---
+++ GoogleTest/GoogleTestEx/GoogleTestEx/GoogleTestEx.cpp
@@ -0,0 +1,33 @@
+// GoogleTestEx.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <gtest/gtest.h>
+
+int Foo(int a, int b)
+{
+	if (a == 0 || b == 0)
+	{
+		throw "don't do that";
+	}
+	int c = a % b;
+	if (c == 0)
+		return b;
+	return Foo(b, c);
+}
+
+TEST(FooTest, HandleNoneZeroInput)
+{
+	EXPECT_EQ(2, Foo(4, 10));
+	EXPECT_EQ(6, Foo(30, 18));
+}
+
+int main(int argc, _TCHAR* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	auto a = RUN_ALL_TESTS();
+	system("pause");
+	return a;
+}
+
